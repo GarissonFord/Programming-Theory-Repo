e0dd995d51e8281000d6a302c5cb5b8c28db9695
Gary DiCiano <garydiciano4@gmail.com>
2021-11-03 15:05:43 -0400

    Basic Functionality Implemented

---
+++ ProgrammingTheoryProject/Assets/Scripts/TitleScreenUIHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TitleScreenUIHandler : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
