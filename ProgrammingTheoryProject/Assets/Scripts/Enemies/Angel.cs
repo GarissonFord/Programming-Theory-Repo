01def23e17a897b50120e2d238f2d724c0e3e9fb
Gary DiCiano <garydiciano4@gmail.com>
2021-11-22 22:19:11 -0500

    Wrapping up enemies for now
    Started to write code for angel movement but am having a hard time getting it to behave the way I want. I'd like for it to stay elevated but move towards the player to attack while they're in range, but for now I'd like to start constructing levels and better optimizing scripts.

--- ProgrammingTheoryProject/Assets/Scripts/Enemies/Angel.cs
+++ ProgrammingTheoryProject/Assets/Scripts/Enemies/Angel.cs
@@ -5,13 +5,29 @@ using UnityEngine;
 public class Angel : Enemy
 {
     [SerializeField] private bool playerInSight;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private bool grounded;
+    private Vector2 originalPosition;
+    GameObject player;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        originalPosition = transform.position;
+    }
 
     protected override void Update()
     {
         base.Update();
 
+        GroundCheck();
         // Regularly check for the player in sight
         animator.SetBool("PlayerInSight", playerInSight);
+    }
+
+    void GroundCheck()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,6 +35,8 @@ public class Angel : Enemy
         if (collision.CompareTag("Player"))
         {
             playerInSight = true;
+            player = collision.gameObject;
+            Attack();
         }
     }
 
@@ -26,5 +44,17 @@ public class Angel : Enemy
     {
         if (collision.CompareTag("Player"))
             playerInSight = false;
+    }
+
+    private void Attack()
+    {
+        Debug.Log("Attacking");
+        rb.MovePosition((player.transform.position - transform.position).normalized);
+    }
+
+    private void ReturnToOriginalPosition()
+    {
+        while (transform.position.y < originalPosition.y)
+            rb.velocity = Vector2.up * (moveSpeed / 2);
     }
 }
